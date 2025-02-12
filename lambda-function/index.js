d72c3f385fcb019ab6128540643803e8fdfded0a
djtwdix <djtwd.ix@icloud.com>
2025-02-12 13:25:54 -0400

    Add basic processing to Lambda function

--- lambda-function/index.js
+++ lambda-function/index.js
@@ -1,5 +1,41 @@
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} = require("@aws-sdk/client-s3");
+
+const s3 = new S3Client({ region: "us-east-1" });
+
 exports.handler = async (event) => {
   console.log("Event:", JSON.stringify(event, null, 2));
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = event.Records[0].s3.object.key;
+
+  const getObjectCommand = new GetObjectCommand({ Bucket: bucket, Key: key });
+  const { Body } = await s3.send(getObjectCommand);
+
+  // Convert stream to string
+  const fileContent = await Body.transformToString();
+
+  const contentObj = JSON.parse(fileContent);
+
+  contentObj.processed = true;
+
+  const putObjectCommand = new PutObjectCommand({
+    Bucket: bucket,
+    Key: key,
+    Body: JSON.stringify(contentObj),
+    ContentType: "application/json",
+  });
+
+  await s3.send(putObjectCommand);
+
+  console.log(
+    "original content: ",
+    JSON.parse(fileContent),
+    " modified content: ",
+    contentObj
+  );
 
   return {
     statusCode: 200,
